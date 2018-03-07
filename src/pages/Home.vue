aeb4917305901baf9f82885e6f6ff51492303a1b
zhoutao <zhoutao@yingroup.com>
2018-03-07 19:40:40 +0800

    init css

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,7 +2,10 @@
   <div class="page home-page">
     <Header :title="'我的备忘录'"></Header>
     <div class="page-con">
-      {{msg}}
+      <ul class="calendar-con">
+        <li v-for="item in weeks" :key="item" class="calendar-item">{{item}}</li>
+        <li v-for="item in days" :key="item" class="calendar-item"><span>{{item}}</span></li>
+      </ul>
     </div>
   </div>
 </template>
@@ -14,14 +17,33 @@ export default {
   name: 'Home',
   data () {
     return {
-      msg: 'hello word'
+      msg: 'hello word',
+      weeks: ['日', '一', '二', '三', '四', '五', '六'],
+      days: []
     }
   },
-  created () {},
+  created () {
+    this.initDays()
+  },
+  methods: {
+    initDays () {
+      const now = new Date()
+      console.log(now)
+    }
+  },
   components: {Header}
 }
 </script>
 
-<style>
-
+<style lang="less">
+.calendar-con {
+  list-style: none;
+  font-size: 0;
+  .calendar-item {
+    display: inline-block;
+    font-size: .7rem;
+    width: 100%/7;
+    text-align: center;
+  }
+}
 </style>
